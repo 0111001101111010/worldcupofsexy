50e5b45a25eb3c23f913aabc82877be44d03a127
DanielHKim <akhisuke@gmail.com>
2014-07-01 19:26:38 -0400

    Fixed $.getJSON to be local rather than from URL

--- web.js
+++ web.js
@@ -26,11 +26,16 @@ app.listen(port, function() {
 var i; 
 var NUM_PLAYERS = 731;
 
-//get all world cup player images by their id in the JSON data and add the
-//image URL as an extra field in each player's JSON
+
 for(i = 0; i <= NUM_PLAYERS; i++)
 {
+    //get all world cup player images by their id in the JSON data and add the
+    //image URL as an extra field in each player's JSON
     var imgURL = "http://img.fifa.com/images/fwc/2014/players/prt-3/" + worldcupdata.idplayer + ".png";
     worldcupdata.imgURL = imgURL;
 
+    //add field for whether the player won or lost the rating round
+    //initially set to an empty string
+    var status = "";
+    worldcupdata.winloss = status;
 }
