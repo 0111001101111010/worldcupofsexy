5a7913d4c241903cc51fea4e4a6be4afd411c873
DanielHKim <akhisuke@gmail.com>
2014-07-01 19:14:35 -0400

    Added constant for number of world cup players

--- web.js
+++ web.js
@@ -24,10 +24,11 @@ app.listen(port, function() {
 });
 
 var i; 
+var NUM_PLAYERS = 731;
 
 //get all world cup player images by their id in the JSON data and add the
 //image URL as an extra field in each player's JSON
-for(i = 0; i <= 731; i++)
+for(i = 0; i <= NUM_PLAYERS; i++)
 {
     var imgURL = "http://img.fifa.com/images/fwc/2014/players/prt-3/" + worldcupdata.idplayer + ".png";
     worldcupdata.imgURL = imgURL;
