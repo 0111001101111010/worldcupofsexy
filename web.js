29e4124da2e6f8664488a1b08d6928b847ed476f
DanielHKim <akhisuke@gmail.com>
2014-07-01 19:29:00 -0400

    Fixed loop so that the new fields are properly added to each player individually

--- web.js
+++ web.js
@@ -15,8 +15,8 @@ var port = parseInt(process.env.PORT, 10) || 5000;
 var worldcupdata;
 
 //get world cup players JSON data from file and parse into object
-$.getJSON('worldcup.json', function(response) {
-    worldcupdata = response;
+$.getJSON('worldcup.json', function(worldcupdata) {
+    console.log(worldcupdata);
 })
 
 app.listen(port, function() {
@@ -32,10 +32,10 @@ for(i = 0; i <= NUM_PLAYERS; i++)
     //get all world cup player images by their id in the JSON data and add the
     //image URL as an extra field in each player's JSON
     var imgURL = "http://img.fifa.com/images/fwc/2014/players/prt-3/" + worldcupdata.idplayer + ".png";
-    worldcupdata.imgURL = imgURL;
+    worldcupdata[i].imgURL = imgURL;
 
     //add field for whether the player won or lost the rating round
     //initially set to an empty string
     var status = "";
-    worldcupdata.winloss = status;
+    worldcupdata[i].winloss = status;
 }
